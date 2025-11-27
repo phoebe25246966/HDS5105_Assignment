3ef7f6958128ea2ce39489bc98b2e4e14f3f8073
Phoebe <P.Wong2@universityofgalway.ie>
2025-11-27 17:34:25 +0000

    3rd commit: added 3rd tab (hospitalization)

--- base/app.R
+++ base/app.R
@@ -18,13 +18,14 @@ ui <- dashboardPage(
       sidebarMenu(
         menuItem("Baseline", tabName = "Baseline", icon = icon("Dashboard")),
         menuItem("Hospitalization", tabName = "Hospitalization",icon = icon("hospital")),
+        menuItem("Mortality", tabName = "Mortality",icon = icon("skull")),
         menuItem("Survival", tabName = "Survival", icon = icon("th"))
       )
     ),
     dashboardBody(
       tabItems(
         tabItem(tabName = "Hospitalization",
-                h2("Hospitalization among different category"),
+                h2("Hospitalization among different categories"),
                 box(
                   selectInput(inputId = "category",label="Select category",choice=c("WHF","Treatment"),multiple = FALSE),
                   selectInput(inputId = "multicategory",label="Show multiple category",choice=c("False","True"),multiple = FALSE),
@@ -34,6 +35,19 @@ ui <- dashboardPage(
                   dataTableOutput("tab2_table1"),
                   box(plotOutput("tab2_distPlot2",height=200,width="100%")),
                   dataTableOutput("tab2_table2"),
+                )
+        ),
+        tabItem(tabName = "Mortality",
+                h2("Mortality among different categories"),
+                box(
+                  selectInput(inputId = "category2",label="Select category",choice=c("CVD","Treatment"),multiple = FALSE),
+                  selectInput(inputId = "multicategory2",label="Show multiple category",choice=c("False","True"),multiple = FALSE),
+                ),
+                fluidRow(
+                  box(plotOutput("tab3_distPlot",height=200)),
+                  dataTableOutput("tab3_table1"),
+                  box(plotOutput("tab3_distPlot2",height=200,width="100%")),
+                  dataTableOutput("tab3_table2"),
                 )
         )
       )
@@ -51,6 +65,23 @@ server <- function(input, output) {
   DIG_df$SEX <- factor(DIG_df$SEX,levels=c(1,2),labels=c("Male","Female"))
   DIG_df$DEATH <- factor(DIG_df$DEATH,levels=c(1,0),labels=c("Death","Alive"))
   
+  DIG_sub2 <- reactive({
+    category_col <- switch(
+      input$category,
+      "WHF" = "WHF",
+      "Treatment" = "TRTMT"
+    )
+    
+    DIG_df %>%
+      #save the categorical variable
+      mutate(category_var = .data[[category_col]]) %>%
+      group_by(category_var) %>% 
+      summarise(
+        hospitalization = sum(HOSP),
+        total = n()
+      )
+  })
+  
   DIG_sub3 <- reactive({
     DIG_df %>%
       group_by(WHF,TRTMT) %>% 
@@ -58,6 +89,45 @@ server <- function(input, output) {
         hospitalization = sum(HOSP),
         total = n()
       )
+  })
+  
+  DIG_sub4 <- reactive({
+    category_col2 <- switch(
+      input$category2,
+      "CVD" = "CVD",
+      "Treatment" = "TRTMT"
+    )
+    
+    DIG_df %>%
+      #save the categorical variable
+      mutate(
+        category_var2 = .data[[category_col2]]) %>%
+      group_by(category_var2) %>% 
+      summarise(
+        death = sum(DEATH == "Death"),
+        total = n()
+      )
+  })
+  
+  DIG_sub5 <- reactive({
+    DIG_df %>%
+      group_by(CVD,TRTMT) %>% 
+      summarise(
+        death = sum(DEATH == "Death"),
+        total = n()
+      )
+  })
+  
+  
+  
+  output$tab2_distPlot <- renderPlot({
+    category <- req(input$category)
+    ggplot(DIG_sub2(),
+           aes(x = factor(category_var), y = hospitalization, fill = factor(category_var))) +
+      geom_col()+labs(x = paste(category),y="Hospitalization",fill= paste(category),title = paste("Plot between ",category," and hospitalization"))
+  })
+  output$tab2_table1 <- renderDataTable({
+    DIG_sub2()
   })
   
   
@@ -70,6 +140,29 @@ server <- function(input, output) {
   })
   output$tab2_table2 <- renderDataTable({
     DIG_sub3()
+  })
+  
+  
+  output$tab3_distPlot <- renderPlot({
+    category2 <- req(input$category2)
+    ggplot(DIG_sub4(),
+           aes(x = factor(category_var2), y = death, fill = factor(category_var2))) +
+      geom_col()+labs(x = paste(category2),y="Death",fill=paste(category2),title = paste("Plot between ",category2," and death"))
+  })
+  output$tab3_table1 <- renderDataTable({
+    DIG_sub4()
+  })
+  
+  
+  output$tab3_distPlot2 <- renderPlot({
+    #req value from input
+    req(input$multicategory2!="False")
+    ggplot(DIG_sub5(), aes(x=factor(CVD), y = death,fill=factor(TRTMT))) +
+      geom_col(position = "dodge") +
+      labs(x="CVD",y="Death",fill="Treatment",title = "Plot between CVD, Death and Treatment")
+  })
+  output$tab3_table2 <- renderDataTable({
+    DIG_sub5()
   })
   
   
